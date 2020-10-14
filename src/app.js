a5a96cdceccbab554d116c3c050386fb430a2287
Michael Oldacre <mloldacre@live.com>
2020-10-14 11:58:04 -0400

    updated gitignore, linted app.js

--- src/app.js
+++ src/app.js
@@ -1,38 +1,38 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
-const authRouter = require('./auth/auth-router')
-const userRouter = require('./user/user-router')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
+const userRouter = require('./user/user-router');
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
-  ? 'tiny'
-  : 'common';
+    ? 'tiny'
+    : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
-})
+    res.send('Hello, world!');
+});
 
-app.use('/api/auth', authRouter)
-app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.use(function errorHandler(error, req, res, next) {
     let response;
-    if(NODE_ENV === 'production') {
-        response = { error: { message: 'server error' } }
+    if (NODE_ENV === 'production') {
+        response = { error: { message: 'server error' } };
     } else {
-        console.error(error)
-        response = { message: error.message, error }
+        console.error(error);
+        response = { message: error.message, error };
     }
-    res.status(500).json(response)
-})
+    res.status(500).json(response);
+});
 
 module.exports = app;
