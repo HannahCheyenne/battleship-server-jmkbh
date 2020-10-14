199c1ab00c66e0f792db0d8d655cb3db9498cdde
Hannah <HannahCheyenne@gmail.com>
2020-10-14 12:00:06 -0500

    added game_stats table to migrations

---
+++ migrations/002.do.create_stats.sql
@@ -0,0 +1,15 @@
+CREATE TABLE "game_stats" (
+    "id" SERIAL PRIMARY KEY,
+    "game_wins" SMALLINT DEFAULT 0,
+    "game_losses" SMALLINT DEFAULT 0,
+    "games_played" SMALLINT DEFAULT 0,
+    "shots_hit" SMALLINT DEFAULT 0,
+    "shots_missed" SMALLINT DEFUALT 0,
+    "carrier_destroyed" SMALLINT DEFAULT 0,
+    "battleship_destroyed" SMALLINT DEFAULT 0,
+    "destroyer_destroyed" SMALLINT DEFAULT 0,
+    "submarine_destroyed" SMALLINT DEFAULT 0,
+    "patrolboat_destroyed" SMALLINT DEFAULT 0,
+    "user_id" INTEGER REFERENCES "user"(id)
+        ON DELETE CASCADE NOT NULL
+);
