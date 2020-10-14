549352cea0eb24582f0944fdb1b58f7aac76d650
Michael Oldacre <mloldacre@live.com>
2020-10-14 17:33:07 -0400

    Fixed small things

--- migrations/002.do.create_stats.sql
+++ migrations/002.do.create_stats.sql
@@ -4,12 +4,12 @@ CREATE TABLE "game_stats" (
     "game_losses" SMALLINT DEFAULT 0,
     "games_played" SMALLINT DEFAULT 0,
     "shots_hit" SMALLINT DEFAULT 0,
-    "shots_missed" SMALLINT DEFUALT 0,
+    "shots_missed" SMALLINT DEFAULT 0,
     "carrier_destroyed" SMALLINT DEFAULT 0,
     "battleship_destroyed" SMALLINT DEFAULT 0,
     "destroyer_destroyed" SMALLINT DEFAULT 0,
     "submarine_destroyed" SMALLINT DEFAULT 0,
     "patrolboat_destroyed" SMALLINT DEFAULT 0,
-    "user_id" INTEGER REFERENCES "user"(id)
+    "user_id" INTEGER REFERENCES "users"(id)
         ON DELETE CASCADE NOT NULL
 );
