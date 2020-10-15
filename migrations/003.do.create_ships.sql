afa7a8da4404958881568293f92067489f18d9c4
Hannah <HannahCheyenne@gmail.com>
2020-10-15 14:11:25 -0500

    added ships table migration NOT WORKING YET, game stats service file built out

---
+++ migrations/003.do.create_ships.sql
@@ -0,0 +1,12 @@
+-- CREATE TABLE "ships" (
+--     "id" SERIAL PRIMARY KEY,
+--     "type" INTEGER
+--     "horizontal" BOOLEAN 
+--     "health" SMALLINT
+--     "player" INTEGER REFERENCES "user"(id)
+--         ON DELETE CASCADE NOT NULL
+-- );
+
+/**
+still working on this data table
+*/
---
+++ src/gamestats/gamestats-service.js
@@ -0,0 +1,40 @@
+const GameStatsService = {
+    getUsersStats(db, user_id) {
+      return db
+        .from('game_stats')
+        .select(
+          'game_stats.id',
+          'game_stats.game_wins',
+          'game_stats.game_losses',
+          'game_stats.games_played',
+          'game_stats.shots_hit',
+          'game_stats.shots_missed',
+          'game_stats.carrier_destroyed',
+          'game_stats.battleship_destroyed',
+          'game_stats.destroyer_destroyed',
+          'game_stats.submarine_destroyed',
+          'game_stats.patrolboat_destroyed',
+          'game_stats.user_id'
+        )
+        .where('game_stats.user_id', user_id)
+        .first();
+    },
+    updateWords(db, data) {
+      const { newWins, newLosses, newPlayed, newHit, newMissed, newCarrier, newBattleship, newDestroyer, newSubmarine, newPatrolboat, user_id } = data
+      return db('game_stats')
+        .where({ user_id: data.user_id })
+        .update({
+          game_wins: data.memory_value,
+          game_losses: data.correct_count,
+          games_played: data.incorrect_count,
+          shots_hit: data.shots_hit,
+          shots_missed: data.shots_missed,
+          carrier_destroyed: data.carrier_destroyed,
+          battleship_destroyed: data.battleship_destroyed,
+          destroyer_destroyed: data.destroyer_destroyed,
+          submarine_destroyed: data.submarine_destroyed,
+          partolboat_destroyed: data.partolboat_destroyed
+        });
+    },
+  };
+  module.exports = GameStatsService;
