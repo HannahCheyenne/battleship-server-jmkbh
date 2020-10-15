d529724a9f7d34961d36f1dc06d651e7fb346506
Hannah <HannahCheyenne@gmail.com>
2020-10-15 16:56:09 -0500

    renamed migration and seed files, added seed data, postman get /api/stats functions

---
+++ seeds/seed.battleship_tables.sql
@@ -0,0 +1,24 @@
+BEGIN;
+
+TRUNCATE 
+    "game_stats",
+    "user";
+
+INSERT INTO "user" ("id","username","name","password")
+VALUES
+    (    
+        1,
+        'admin',
+        'Dunder Mifflin Admin',
+        -- password = "pass"
+        '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+    );
+
+INSERT INTO "game_stats" ("id","game_wins", "game_losses", "games_played", "shots_hit", "shots_missed", "carrier_destroyed", "battleship_destroyed", "destroyer_destroyed", "submarine_destroyed", "patrolboat_destroyed", "user_id")
+VALUES
+    (1, 2, 2, 4, 25, 2, 2, 3, 2, 3, 2, 1);
+
+SELECT setval('game_stats_id_seq', (SELECT MAX(id) from "game_stats"));
+SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+
+COMMIT;
--- src/server.js
+++ src/server.js
@@ -10,4 +10,6 @@ const db = knex({
 
 app.set('db', db);
 
-app.listen(PORT);
+app.listen(PORT, () => {
+    console.log(`Server listening at http://localhost:${PORT}`)
+});
