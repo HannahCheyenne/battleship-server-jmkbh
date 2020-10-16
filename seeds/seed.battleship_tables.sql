323143543463933e06c1a4ac19f733f707527f62
Jackie Abert <jax.abert@gmail.com>
2020-10-16 17:03:40 -0400

    created more mock data

--- seeds/seed.battleship_tables.sql
+++ seeds/seed.battleship_tables.sql
@@ -11,12 +11,19 @@ VALUES
         'admin',
         'Dunder Mifflin Admin',
         -- password = "pass"
-        '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+        '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'),
+(
+        2,
+        'demo',
+        'demo account',
+        -- password = "pass",
+    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
     );
 
 INSERT INTO "game_stats" ("id","game_wins", "game_losses", "games_played", "shots_hit", "shots_missed", "carrier_destroyed", "battleship_destroyed", "destroyer_destroyed", "submarine_destroyed", "patrolboat_destroyed", "user_id")
 VALUES
-    (1, 2, 2, 4, 25, 2, 2, 3, 2, 3, 2, 1);
+    (1, 2, 2, 4, 25, 2, 2, 3, 2, 3, 2, 1),
+    (2, 2, 2, 4, 25, 2, 2, 3, 2, 3, 2, 2);
 
 SELECT setval('game_stats_id_seq', (SELECT MAX(id) from "game_stats"));
 SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
