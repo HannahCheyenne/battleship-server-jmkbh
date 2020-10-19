c1de0ec6250536ea453f475a5243a970bb2605c2
Brett Westerlund <drbretto82@gmail.com>
2020-10-19 17:34:55 -0400

    fixed broken stuff

--- src/game/game-router.js
+++ src/game/game-router.js
@@ -1,39 +1,39 @@
-const path = require('path')
-const express = require('express')
+// const path = require('path')
+// const express = require('express')
 
-const GameService = require('./game-service')
-const { requireAuth } = require('../middleware/jwt-auth')
-const gameRouter = express.Router()
-const jsonBodyParser = express.json()
+// const GameService = require('./game-service')
+// const { requireAuth } = require('../middleware/jwt-auth')
+// const gameRouter = express.Router()
+// const jsonBodyParser = express.json()
 
-gameRouter
-  .route('/:id')
-  .get(async (req, res, next) => {
-    try {
-      const gameState = await GameService.getGameState(
-        req.app.get('db'))
-      res.json({
-        gameState
-      })
-      next()
-    } catch (error) {
-      next(error)
-    }
-  })
+// gameRouter
+//   .route('/:id')
+//   .get(async (req, res, next) => {
+//     try {
+//       const gameState = await GameService.getGameState(
+//         req.app.get('db'))
+//       res.json({
+//         gameState
+//       })
+//       next()
+//     } catch (error) {
+//       next(error)
+//     }
+//   })
 
-  .route('/board')
-  .get(async (req, res, next) => {
-    try {
-      const gameState = await GameService.getGameBoard(
-        req.app.get('db'))
-      res.json({
-        gameBoard
-      })
-      next()
-    } catch (error) {
-      next(error)
-    }
-  })
-  .post(asyn ())
+//   .route('/board')
+//   .get(async (req, res, next) => {
+//     try {
+//       const gameState = await GameService.getGameBoard(
+//         req.app.get('db'))
+//       res.json({
+//         gameBoard
+//       })
+//       next()
+//     } catch (error) {
+//       next(error)
+//     }
+//   })
+//   .post(async ())
 
-module.exports = gameStatsRouter
+// module.exports = gameStatsRouter
--- src/game/game-service.js
+++ src/game/game-service.js
@@ -1,20 +1,20 @@
-const GameService = {
-  getGameState(db) {
-    return db
-      .from('game_state')
-      .select(
-        'p1_board',
-        'p2_board',
-        'p1_health',
-        'p1_health',
-        'player_turn',
-        'active_game',
-      )
-  },
+// const GameService = {
+//   getGameState(db) {
+//     return db
+//       .from('game_state')
+//       .select(
+//         'p1_board',
+//         'p2_board',
+//         'p1_health',
+//         'p1_health',
+//         'player_turn',
+//         'active_game',
+//       )
+//   },
   
-  constructArrays
+//   //constructArrays
 
 
 
-};
-module.exports = GameService;
+// };
+// module.exports = GameService;
