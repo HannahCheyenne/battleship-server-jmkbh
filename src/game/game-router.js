3fbfd2b96cc4d9e723ce494bc16343e5dccd486c
Brett Westerlund <drbretto82@gmail.com>
2020-10-20 13:57:32 -0400

    basic post test complete

--- src/game/game-router.js
+++ src/game/game-router.js
@@ -3,19 +3,50 @@ const express = require("express");
 
 const GameService = require("./game-service");
 const { requireAuth } = require("../middleware/jwt-auth");
+const { postGameState } = require("./game-service");
 const gameRouter = express.Router();
 const jsonBodyParser = express.json();
 
-gameRouter.route("/:id").get(async (req, res, next) => {
-  try {
-    const gameState = await GameService.getGameState(req.app.get("db"), req.params.id);
-    res.json({
-      gameState,
-    });
-    next();
-  } catch (error) {
-    next(error);
-  }
-});
+gameRouter
+  .route("/:id")
+  .get(jsonBodyParser, async (req, res, next) => {
+    try {
+      const gameState = await GameService.getGameState(
+        req.app.get("db"),
+        req.params.id
+      );
+      res.json({
+        gameState,
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  })
+  .post(jsonBodyParser, async (req, res, next) => {
+    try {
+      const gameState = await GameService.getGameState(
+        req.app.get("db"),
+        req.params.id
+      );
+      const { x, y } = req.body;
+
+      console.log("y", y);
+      console.log("x", x);
+      console.log("gameState", gameState[0].p1_board);
+
+      gameState[0].p1_board[y][x] = 8;
+
+      //replace this with magic later
+      GameService.postGameState(req.app.get("db"), gameState);
+
+      res.json({
+        gameState,
+      });
+      next();
+    } catch (error) {
+      next(error);
+    }
+  });
 
 module.exports = gameRouter;
--- src/game/game-service.js
+++ src/game/game-service.js
@@ -16,9 +16,40 @@ const GameService = {
       )
   },
   
+  postGameState(db, newState) {
+
+    return db
+    .insert(newState)
+    .into('game_state')
+    .returning('*')
+
+},
+
+
+postNewGame(db){  
+  
+  
+  return db
+  .from('game_state')
+  .select(
+    'id',
+    'p1_board',
+      'p2_board',
+      'p1_health',
+      'p2_health',
+      'player_turn',
+      'active_game',
+      )
+    .where(
+      'id', id
+    )
+  }
+
+  
   //constructArrays
-
-
-
+  
 };
+
+
+
 module.exports = GameService;
