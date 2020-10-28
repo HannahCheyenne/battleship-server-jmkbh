cffc2b3b707d79ee8dec68257702a96cad2faaa6
Michael Oldacre <mloldacre@live.com>
2020-10-28 16:50:01 -0400

    Reverted back to working state

--- src/game/game-router.js
+++ src/game/game-router.js
@@ -36,6 +36,7 @@ gameRouter
       const { x, y } = req.body;
       //TODO validateMove(gameBoard, x,y)
       gameState = Game.checkHit(gameState, x, y);
+      //console.log("newState", gameState);
       gameState.player_turn = false;
       await Game.postGameState(req.app.get("db"), gameState);
       res.json({
@@ -50,7 +51,9 @@ gameRouter
 gameRouter.route("/newgame").post(jsonBodyParser, async (req, res, next) => {
   try {
     const newGame = req.body;
+    //console.log("req.body", req.body)
     const id = await Game.initializeGame(req.app.get("db"), newGame);
+    //console.log("id", id)
     const rawState = await Game.getGameState(req.app.get("db"), id[0]);
     const gameState = rawState[0];
     res.json({
