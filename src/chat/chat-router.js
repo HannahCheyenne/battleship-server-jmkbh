24b91fcc4cb24bbd396a391d3c161517993f5377
Michael Oldacre <mloldacre@live.com>
2020-10-29 17:55:57 -0400

    More progress

--- src/chat/chat-router.js
+++ src/chat/chat-router.js
@@ -26,8 +26,8 @@ io.on('connect', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.` });
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
+        socket.emit('message', { user: 'Adm:', text: `${user.name}, welcome to room ${user.room}.` });
+        socket.broadcast.to(user.room).emit('message', { user: 'Adm:', text: `${user.name} has joined!` });
 
         io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
@@ -96,9 +96,11 @@ io.on('connect', (socket) => {
         callback();
     });
 
-    socket.on('disconnect', () => {
+    socket.on('disconnect', function() {
+        console.log('SERVER DISCONNECT');
+        socket.disconnect();
         const user = removeUser(socket.id);
-
+        
         if (user) {
             io.to(user.room).emit('message', { user: 'Adm:', text: `${user.name} has left.` });
             io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
--- src/chat/chat-users.js
+++ src/chat/chat-users.js
@@ -19,6 +19,7 @@ const addUser = ({ id, name, room }) => {
 
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id);
+    console.log('REMOVING USER');
 
     if (index !== -1) return users.splice(index, 1)[0];
 };
