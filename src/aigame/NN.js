44a428b515e2d6feeb0800db70a4c9d1293d03f3
Brett Westerlund <drbretto82@gmail.com>
2020-12-02 13:44:17 -0500

    started Neural Network class

---
+++ src/aigame/NN.js
@@ -0,0 +1,89 @@
+const math = require("mathjs");
+
+function zeroes(x, y) {
+  let zeroed = [];
+  for (let i = 0; i < y; i++) {
+    let tempArray = [];
+    for (let j = 0; j < x; j++) {
+      tempArray.push(0);
+    }
+    zeroed.push(tempArray);
+  }
+  return zeroed;
+}
+
+function randomizeArray(x, y) {
+  let output = zeroes(x, y);
+  for (let i = 0; i < y; i++) {
+    for (let j = 0; j < x; j++) {
+      output[i][j] = Math.random();
+    }
+  }
+  return output;
+}
+
+function sigmoid(arr) {
+  return 1 / (1 + Math.pow(Math.E, -t));
+}
+
+class NeuralNetwork {
+  constructor(x, y) {
+    this.input = x;
+    this.width = x._data.length;
+    this.length = x._data[0].length;
+    this.weights1 = math.matrix(randomizeArray(this.length, this.width));
+    this.weights2 = math.matrix(randomizeArray(this.length, 1));
+    this.y = y;
+    this.output = math.matrix(zeroes(this.length, this.width));
+  }
+
+  feedforward() {
+    this.layer1 = sigmoid(math.dot(this.input, this.weights1));
+    this.output = sigmoid(math.dot(this.layer1, this.weights2));
+  }
+
+  backprop() {
+    // # application of the chain rule to find derivative of the loss function with respect to weights2 and weights1
+    d_weights2 = np.dot(
+      this.layer1.T,
+      2 * (this.y - this.output) * sigmoid_derivative(this.output)
+    );
+    d_weights1 = np.dot(
+      this.input.T,
+      np.dot(
+        2 * (this.y - this.output) * sigmoid_derivative(this.output),
+        this.weights2.T
+      ) * sigmoid_derivative(this.layer1)
+    );
+
+    // # update the weights with the derivative (slope) of the loss function
+    this.weights1 += d_weights1;
+    this.weights2 += d_weights2;
+  }
+}
+
+function main() {
+  let input = math.matrix([
+    [1, 2],
+    [3, 4],
+    [6, 6],
+  ]);
+  let output = [[1], [2], [3]];
+  let brain = new NeuralNetwork(input, output);
+
+  console.log("");
+  console.log(
+    "SUMMARY: -----------------------------------------------------------"
+  );
+  console.log("Input layer:", brain.input._data);
+  console.log("weights 1:", brain.weights1._data);
+  console.log("weights 2:", brain.weights2._data);
+  console.log("y:", brain.input._data);
+  console.log("output?", brain.output._data);
+  console.log(
+    "END SUMMARY: -----------------------------------------------------------"
+  );
+  console.log("");
+}
+
+main();
